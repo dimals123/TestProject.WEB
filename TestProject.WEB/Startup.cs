e847424c3b1b10b33174325fbff637b61ef405d2
Дмитрий <dimals97@hotmail.com>
2021-03-27 19:49:36 +0300

    start project

--- TestProject.WEB/Startup.cs
+++ TestProject.WEB/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -14,6 +9,7 @@ namespace TestProject.WEB
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -27,10 +23,9 @@ namespace TestProject.WEB
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
