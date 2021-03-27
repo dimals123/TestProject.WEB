155eb422b02faefed55158f8a77a72a25ebf3899
Дмитрий <dimals97@hotmail.com>
2021-03-27 21:23:58 +0300

    add db connection string

---
+++ TestProject.BusinessLogic/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestProject.DataAccess;
+
+namespace TestProject.BusinessLogic.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void DependencyInjectionConnectionConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TestProjectContext>(options => options.UseSqlServer(connectionString));
+        }
+    }
+}
--- TestProject.WEB/Startup.cs
+++ TestProject.WEB/Startup.cs
@@ -1,15 +1,25 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TestProject.BusinessLogic.Configurations;
 
 namespace TestProject.WEB
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.DependencyInjectionConnectionConfig(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
